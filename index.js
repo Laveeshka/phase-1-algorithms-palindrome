b74aa5aea4eb0cb4b4686403cebc7e778ef5049e
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-09-29 23:07:34 +1000

    completed lab

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const wordArray = word.split('');
+  const wordLength = wordArray.length;
+
+  for (let i = 0; i < Math.round(wordLength/2); i++){
+    if (wordArray[i] !== wordArray[(wordLength - 1) - i])
+      return false;
+  }
+  return true;
 }
 
 /* 
   Add your pseudocode here
+  Convert the string into an array
+  Iterate over the string array until halfway
+  Compare two indices: index i that starts at 0 and index j that starts at the last index
+  Index i will increment by 1
+  Index j will decrement by 1
+  Match element at index i with element at index j
+  return false as soon as there is not a match
+  Stop comparing when i = j and return true if the elements match
 */
 
 /*
